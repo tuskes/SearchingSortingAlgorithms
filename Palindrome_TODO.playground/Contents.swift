3a9b54484325090c635789ed33c2cce417e24030
Christian Tuskes <christian.tuskes@gmail.com>
2016-05-03 13:00:11 -0700

    completed Palindrome.playground

--- Palindrome_TODO.playground/Contents.swift
+++ Palindrome_TODO.playground/Contents.swift
@@ -1,23 +1,26 @@
 //A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. Allowances may be made for adjustments to capital letters, punctuation, and word dividers.
 
-//TODO:
-//1. make letters all one case (lower or upper)
-//2. remove spaces and special characters
-
-let p = "neveroddoreven"
-let length = p.characters.count
-let halfLength = length/2
 var isPalindrome = true
+var p = "Never Odd or Even"
+let plowercase = p.lowercaseString
+let pNoSpaces = String(plowercase.characters.filter {$0 != " "})    //removes spaces
+let length = pNoSpaces.characters.count
+let halfLength = length/2
+
+print("\(p)")
+print("\(plowercase)")
+print("\(pNoSpaces)")
+print("\n")
 
 for i in 0...halfLength {
     
-    print("\(i):\(String(p[p.characters.startIndex.advancedBy(i)]))")
-    print("\(length-i-1):\(String(p[p.characters.startIndex.advancedBy(length-i-1)]))")
+    print("\(i):\(String(pNoSpaces[pNoSpaces.characters.startIndex.advancedBy(i)]))")
+    print("\(length-i-1):\(String(pNoSpaces[pNoSpaces.characters.startIndex.advancedBy(length-i-1)]))")
     print("\n")
     
-    if (String(p[p.characters.startIndex.advancedBy(i)]) != String(p[p.characters.startIndex.advancedBy(length-i-1)])) {
+    if (String(pNoSpaces[pNoSpaces.characters.startIndex.advancedBy(i)]) != String(pNoSpaces[pNoSpaces.characters.startIndex.advancedBy(length-i-1)])) {
         isPalindrome = false
-        print(String(p[p.characters.startIndex.advancedBy(length-i-1)]))
+        print(String(pNoSpaces[pNoSpaces.characters.startIndex.advancedBy(length-i-1)]))
         break
     }
 }
